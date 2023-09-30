91c9af47f5a62f04efcd025c3c9d176531cef668
Shobhan <shobhan.kumar@gmail.com>
2023-09-29 18:43:50 -0700

    Update goroutines.go

--- goroutines.go
+++ goroutines.go
@@ -1,3 +1,13 @@
+// This file show cases how to use following
+// go routines
+// anonymous function
+// Waitgroups
+// Mutexs
+// mutexes are need as below example has a slice thats modified by various go routines (i.e threads)
+// since slice is being appened by multiple goroutines, need to safegaurd slice using a mutex
+// when we have goroutines, waitgroups also needed as main thread can exit before even
+// goroutines start executing
+
 package main
 
 import (
